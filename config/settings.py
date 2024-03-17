bbfc1779388bb073f07a54f257dfb8d5d5c618db
duraprakash <duraprakash141@gmail.com>
2024-03-17 16:20:14 +0545

    debug off and allowed_host in settings set

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-28-a)td4_r!)9xd1qi(&0_6daf6!ltf_ve+9su2nk^w*at2(h6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
